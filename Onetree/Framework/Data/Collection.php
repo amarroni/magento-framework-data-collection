4774d940c4b6d39c4ddc477ec7a62016e129c818
Alejandro Marroni <alegringus@gmail.com>
2019-04-18 02:19:59 -0300

    adding module code

---
+++ Onetree/Framework/Data/Collection.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Onetree\Framework\Data;
+
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Data collection
+ *
+ * TODO: Refactor use of \Magento\Framework\Option\ArrayInterface in library.
+ *
+ * @api
+ */
+class Collection
+{
+    /**
+     * Adding item to item array
+     *
+     * @param \Magento\Eav\Model\Entity\Collection\AbstractCollection $subject
+     * @param \Closure $process
+     * @param \Magento\Framework\DataObject $object
+     * @return $this
+     */
+    public function aroundAddItem(\Magento\Eav\Model\Entity\Collection\AbstractCollection $subject, \Closure $process, \Magento\Framework\DataObject $object)
+    {
+        try{
+            return $process($object);
+        }catch ( \Exception $e){
+            return $this;
+        }
+
+    }
+
+}
---
+++ Onetree/Framework/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright Copyright © 2018 Onetree. All rights reserved.
+ * @author    Hector Perez Hernandez <hector.perez@onetree.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Onetree_Framework',
+    __DIR__
+);
